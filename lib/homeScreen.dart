fd13c6a66627fcf39e8abf07df53fb37fa699120
zrezke <jeretina.filip@gmail.com>
2020-10-31 00:43:48 +0100

    Added dropdown menues into drawer for filtering the search, and a reset button to reset filters.

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -14,7 +14,35 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+  Map applyedFilters = {};
   List searchResults = ["N1", "N2"];
+  Map searchFilterItems = {
+    "Search in": [
+      "LibGen(Sci-Tech)",
+      "Scientific articles",
+      "Fiction",
+      "Comics",
+      "Standards",
+      "Magazines"
+      ],
+      "Search with mask (word*)": [
+        "No",
+        "Yes"
+        ],
+      "Search in fields": [
+        "The column set to default",
+        "Title",
+        "Author(s)",
+        "Series",
+        "Publisher",
+        "Year",
+        "ISBN",
+        "Language",
+        "MD5",
+        "Tags",
+        "Extension"
+      ]
+    };
 
   SliverChildBuilderDelegate searchResultListView(
       BuildContext context, List searchResults) {
@@ -30,6 +58,51 @@ class _HomeScreenState extends State<HomeScreen> {
     }, childCount: searchResults.length);
   }
 
+  List <DropdownMenuItem <String>> _makeDropdownMenuItems(String key) {
+    List <DropdownMenuItem <String>> items = [];
+    for (int i=0; i< searchFilterItems[key].length; i++) {
+      String val = searchFilterItems[key][i];
+      DropdownMenuItem <String> item = DropdownMenuItem(
+        value: val,
+        child: Text(
+          val
+        )
+      );
+      items.add(item);
+    }
+    return items; 
+  }
+
+  void _getHint(hint, String key, int idx) {
+    if (hint == null) {
+      hint = searchFilterItems[key][0];
+    }
+    else {
+      hint = hint;
+    }
+    return hints.replaceRange(idx, idx+1, [hint]);
+  }
+
+  List hints = [null, null, null];
+
+  DropdownButton searchFiltersBuilder(BuildContext context, int idx) {
+    String key = searchFilterItems.keys.toList()[idx];
+    _getHint(hints[idx], key, idx);
+    var hint = hints[idx];
+    return DropdownButton(
+      key: Key(idx.toString()),
+      hint: Text(hint),
+      items: _makeDropdownMenuItems(key),
+      value: null,
+      onChanged: (value) {
+        applyedFilters[key] = value;
+        setState(() {
+          hints[idx] = value;
+        });
+      },
+    );
+  }
+
   Drawer searchFilterDrawer(BuildContext context) {
     return Drawer(
     child: Column(
@@ -42,6 +115,39 @@ class _HomeScreenState extends State<HomeScreen> {
             fit: BoxFit.cover,
             )
         ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 20, 0, 0)
+        ),
+        Divider(),
+        Card(
+          child: ListTile(
+            title: Text("Search in:"), 
+            subtitle: searchFiltersBuilder(context, 0),
+          )
+        ),
+        Divider(),
+        Card(
+          child: ListTile(
+            title: Text("Search with mask (word*)"), 
+            subtitle: searchFiltersBuilder(context, 1),
+          ),
+        ),
+        Divider(),
+        Card(
+          child: ListTile(
+            title: Text("Search in fields"), 
+            subtitle: searchFiltersBuilder(context, 2),
+          )
+        ),
+        FlatButton(
+          color: Colors.amber,
+          child: Text("Reset to default"),
+          onPressed: () {
+            setState(() {
+              hints = [null, null, null];
+            });
+          },
+        )
       ],
     ),
   );
